1d473399751ddf08004547610d3dc8cb5c1218de
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-04-11 14:57:33 +0100

    working tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,15 @@
-def fizzbuzz
+def fizzbuzz(number)
+  (1..number).map do |num|
+    if num % 3 == 0
+      'fizz'
+    elsif num % 5 == 0
+      'buzz'
+    elsif num % 3 == 0 && num % 5 == 0 
+      'fizzbuzz'
+    else
+      num
+    end
+  end
 end
+
+fizzbuzz(21)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,15 @@ require_relative '../lib/fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq [1, 2, 'fizz']
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq [1, 2, 'fizz', 4, 'buzz']
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizz"]
   end
 end
 
